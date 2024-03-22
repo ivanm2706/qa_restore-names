27fe3e76e34108c59773e0e10b1bf3a1eac82aca
ivanm2706 <ivanmorhun664@gmail.com>
2024-03-22 13:59:24 -0500

    added tests for function restoreNames

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,40 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should declare', () => {
+    expect(restoreNames).toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it('should restore firstName field', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    const expectedResult = [
+      {
+        firstName: 'Jack',
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        firstName: 'Mike',
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toEqual(expectedResult);
+  });
 });
